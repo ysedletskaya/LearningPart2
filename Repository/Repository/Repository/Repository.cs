ba69011b6c5a174f0335e8f7b231e7addc1f564c
ysedletskaya <ysedletskaya@gmail.com>
2016-07-14 17:07:41 +0300

    Repository initial

---
+++ Repository/Repository/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        internal Table<T> DataTable;
+
+        public Repository(DataContext dataContext)
+        {
+            DataTable = dataContext.GetTable<T>();
+        }
+
+
+        public void Insert(T entity)
+        {
+            DataTable.InsertOnSubmit(entity);
+        }
+
+
+        public void Delete(T entity)
+        {
+            DataTable.DeleteOnSubmit(entity);
+        }
+
+        public System.Linq.IQueryable<T> SearchFor(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return DataTable.Where(predicate);
+        }
+
+        public System.Linq.IQueryable<T> GetAll()
+        {
+            return DataTable;
+        }
+    }
+}
